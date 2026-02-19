77a226cb6926969e561c96078812383504a6e78b
Xinyue Lu <i@7086.in>
2026-02-18 21:35:12 -0800

    Fix 'c'

--- lib/sober_hy/decoder.rb
+++ lib/sober_hy/decoder.rb
@@ -5,10 +5,10 @@ module SoberHy
     KEY_MAP = {
       0x01C9B463 => 'n', 0x54AEAE1A => 'r', 0xAF2D6B89 => 'h',
       0x14D5978E => 'm', 0x5D2970BF => '7', 0xB807DA49 => 'p',
-      0x22A48B6B => 'w', 0x65D52263 => 'f', 0xBD84094C => '9',
-      0x2CBC6846 => 'a', 0x665493DF => 't', 0xC0A35B08 => '4',
-      0x474812AA => '5', 0x819B4227 => 'k', 0xD80D9A8C => 'y',
-      0x525BD604 => 'e', 0x82CB7BE3 => 'j', 0xFA0E1C5F => 'c',
+      0x22A48B6B => 'w', 0x65D52263 => 'f', 0xB9FD119D => 'c',
+      0x2CBC6846 => 'a', 0x665493DF => 't', 0xBD84094C => '9',
+      0x474812AA => '5', 0x819B4227 => 'k', 0xC0A35B08 => '4',
+      0x525BD604 => 'e', 0x82CB7BE3 => 'j', 0xD80D9A8C => 'y',
       0x540C91BF => 'x', 0xAEC788C3 => 's', 0xFD05CC2C => '6'
     }.freeze
 
